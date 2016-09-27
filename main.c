ebd0cd4de1451066a30a853713af273fd7e61ad6
Matias Brizuela <brizuela_matias1@hotmail.com>
2016-09-27 20:21:57 -0300

    El main.c contiene un código básico

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "stdio.h"         /* <= own header */
+
+int main(void){
+
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while(1) {
+
+      delay(1000);
+
+   }
+
+   /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+      por ningun S.O. */
+   return 0 ;
+}
+
+/*==================[end of file]============================================*/
